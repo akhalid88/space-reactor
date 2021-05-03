a959867fb1b4a8599618198db8895c3ecce0e7b8
Muhammad K <akhalid.code@gmail.com>
2021-05-03 13:29:18 -0700

    design simple front-end page using react and material-ui

---
+++ src/static/place.js
@@ -0,0 +1,16 @@
+const places = [
+	{
+		title: 'Earth',
+		description: "The little blue spec called home",
+		imageUrl: process.env.PUBLIC_URL + '/assets/earth.jpg',
+		time: 1500,
+	},
+	{
+		title: 'Moon',
+		description: "Our friendly grey neighbor",
+		imageUrl: process.env.PUBLIC_URL + '/assets/moon.jpg',
+		time: 1500,
+	},
+];
+
+export default places;
